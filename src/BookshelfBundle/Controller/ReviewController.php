8a5b43261dc3929892374b12c46967891e7f0f77
Wojtek <wojtek.lenartowicz@gmail.com>
2015-11-01 21:46:25 +0100

    Added created, delete and show functionalities to reviews.

--- src/BookshelfBundle/Controller/ReviewController.php
+++ src/BookshelfBundle/Controller/ReviewController.php
@@ -2,9 +2,11 @@
 
 namespace BookshelfBundle\Controller;
 
+use BookshelfBundle\Entity\Review;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class ReviewController
@@ -14,45 +16,87 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class ReviewController extends Controller
 {
     /**
-     * @Route("/show")
+     * @Route("/show/{id}")
      * @Template()
      */
-    public function showAction()
+    public function showAction($id)
     {
+        $repo = $this->getDoctrine()->getRepository("BookshelfBundle:Review");
+        $review = $repo->find($id);
+
         return array(
-                // ...
+                "review" => $review
             );    }
 
     /**
-     * @Route("/createForm")
+     * @Route("/createForm/{bookId}")
      * @Template()
      */
-    public function createFormAction()
+    public function createFormAction($bookId)
     {
+        $form = $this->createFormBuilder()
+            ->add("subject", "text")
+            ->add("review", "textarea")
+            ->add("save", "submit")
+            ->getForm();
+
         return array(
-                // ...
+                "form" => $form->createView(),
+                "bookId" => $bookId
             );
     }
 
     /**
-     * @Route("/create")
-     * @Template()
+     * @Route("/create/{bookId}")
      */
-    public function createAction()
+    public function createAction(Request $request, $bookId)
     {
-        return array(
-                // ...
-            );
+        $newReview = new Review();
+
+        $form = $this->createFormBuilder($newReview)
+            ->add("subject", "text")
+            ->add("review", "textarea")
+            ->add("save", "submit")
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        $validator = $this->get("validator");
+        $errors = $validator->validate($form);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($newReview);
+
+        $repo = $this->getDoctrine()->getRepository("BookshelfBundle:Book");
+        $book = $repo->find($bookId);
+        $book->addReview($newReview);
+
+        $newReview->setBook($book);
+
+        $em->flush();
+
+        if($form->isValid()){
+            return $this->redirectToRoute("bookshelf_book_show", array("id" => $bookId));
+        } else {
+            return $this->render("BookshelfBundle:Review:createForm.html.twig", array("bookId" => $bookId, "form" => $form, "errors" => $errors));
+        }
     }
 
     /**
-     * @Route("/delete")
-     * @Template()
+     * @Route("/delete/{id}")
      */
-    public function deleteAction()
+    public function deleteAction($id)
     {
-        return array(
-                // ...
-            );    }
+        $repo = $this->getDoctrine()->getRepository("BookshelfBundle:Review");
+        $review = $repo->find($id);
+
+        $bookId = $review->getBook()->getId();
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($review);
+        $em->flush();
+
+        return $this->redirectToRoute("bookshelf_book_show", array("id" => $bookId));
+    }
 
 }
